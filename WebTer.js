90c9c08921acb619ee2592d8c0e925a68fd58945
Eszti2222 <nagy.eszter2222@gmail.com>
2025-04-10 12:14:52 +0200

    webter Kosaram megjelenit

--- WebTer.js
+++ WebTer.js
@@ -3,20 +3,25 @@ import Kosaram from "./Kosaram.js";
 
 export default class WebTer {
     #lista = []
-    constructor(szElemAS, szElemART, lista){
+    constructor(szElemAS, szElemART, lista) {
         this.szElemAS = szElemAS,
-        this.szElemART = szElemART,
-        this.#lista = lista,
-        this.megjelenit(),
-        this.megjelenitListaElemek()
+            this.szElemART = szElemART,
+            this.#lista = lista,
+            this.megjelenit(),
+            this.megjelenitListaElemek()
     }
 
-    megjelenit(){
-
+    megjelenitKosar() {
+        this.szElemAS.innerHTML = "";
+        for (let index = 0; index < this.#lista.length; index++) {
+            const termek = this.#lista[index];
+            new Kosaram(kosaram.szElem,this.szElemAS);
+        }
     }
 
+    
     megjelenitListaElemek() {
-        this.szElemART.innerHTML = ""; 
+        this.szElemART.innerHTML = "";
         for (let index = 0; index < this.#lista.length; index++) {
             const termek = this.#lista[index];
             new Termek(termek.elUt, termek.ar, index, termek.termekNev, this.szElemART);
